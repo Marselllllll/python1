37559ec8a3c07bf993a2905ec3216f239586e051
Marselllllll <night.square679@mail.ru>
2022-11-01 03:20:03 +0300

    complete task

--- Tasks/classsss.py
+++ Tasks/classsss.py
@@ -1,5 +1,5 @@
-from Geometry import birthDAY as br
-from Geometry import parMontiHola as pmh
+from library import birthDAY as br
+from library import parMontiHola as pmh
 
 print(pmh.monty_hall(10000))
 print(br.birthday(1000, 60))
