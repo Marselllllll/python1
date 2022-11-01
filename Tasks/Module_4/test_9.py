3ee30bdfd88dd02a7b05de3398636c4baf23357a
Marselllllll <night.square679@mail.ru>
2022-11-01 06:00:45 +0300

    complete task

--- Tasks/Module_4/test_9.py
+++ Tasks/Module_4/test_9.py
@@ -1,2 +1,9 @@
-a, b, c = int(input('Введите 3 числа через пробел: ', sep=' '))
-print(a,b,c)
+a, b, c = map(int, input('Введите три числа через пробел: ').split())
+n = [a, b, c]
+aa = n.count(a)
+bb = n.count(b)
+cc = n.count(c)
+if max(aa, bb, cc) == 1:
+    print(0)
+else:
+    print(max(aa, bb, cc))
