5c5001358a5ba4d0ffebacd229b7ccdd3dd0a9a2
welkimoon <ivanova.margarita.2004@gmail.com>
2022-11-22 15:26:46 +0300

    complete

--- Tasks/Practical/Practical_5.py
+++ Tasks/Practical/Practical_5.py
@@ -14,14 +14,15 @@ os.path.splitext(filename)
 extension_tuple = os.path.splitext(filename)
 extension_list = ''.join(extension_tuple)
 
+text = []
 # Проверка расширения
-if extension_list.lower().endswith('.txt') == True:
+if extension_list.lower().endswith('.txt'):
     # Считывание файла
     try:
-        text = open(extension_list, mode='r', encoding='UTF-8')
+        text = open(extension_list, mode='r')
         check_count_lines = text.readlines()
         line2 = check_count_lines[1]
-    except:
+    except IndexError:
         print('\033[31m\033[1mОшибка форматирования.\033[0m\nВ файле менее \033[31m\033[1m2-х строк.\033[0m')
     else:
         text.seek(0)
@@ -41,20 +42,20 @@ if extension_list.lower().endswith('.txt') == True:
                 for j in value_line_list:
                     j = j.lower()
                     if j == ' ':
-                        # check_end = 1
-                        print(f'\033[31m\033[1mОшибка форматирования.\033[0m\nВ строке под номером {line} есть символ \033[31m\033[1m"Пробел".\033[0m')
+                        print(f'\033[31m\033[1mОшибка форматирования.\033[0m\n'
+                              f'В строке под номером {line} есть символ \033[31m\033[1m"Пробел".\033[0m')
                         break
                     elif alfavit.russian.count(j) != 0:
-                        # check_end = 1
-                        print(f'\033[31m\033[1mОшибка форматирования.\033[0m\nВ строке под номером {line} есть символ \033[31m\033[1mРусского алфавита.\033[0m')
+                        print(f'\033[31m\033[1mОшибка форматирования.\033[0m\n'
+                              f'В строке под номером {line} есть символ \033[31m\033[1mРусского алфавита.\033[0m')
                         break
                     elif alfavit.english.count(j) != 0:
-                        # check_end = 1
-                        print(f'\033[31m\033[1mОшибка форматирования.\033[0m\nВ строке под номером {line} есть символ \033[31m\033[1mАнглийского алфавита.\033[0m')
+                        print(f'\033[31m\033[1mОшибка форматирования.\033[0m\n'
+                              f'В строке под номером {line} есть символ \033[31m\033[1mАнглийского алфавита.\033[0m')
                         break
                     else:
-                        # check_end = 1
-                        print(f'\033[31m\033[1mОшибка форматирования.\033[0m\nВ строке под номером {line} есть \033[31m\033[1mНебуквенный символ.\033[0m')
+                        print(f'\033[31m\033[1mОшибка форматирования.\033[0m\n'
+                              f'В строке под номером {line} есть \033[31m\033[1mНебуквенный символ.\033[0m')
                         break
 
             else:
@@ -62,9 +63,9 @@ if extension_list.lower().endswith('.txt') == True:
         finish_count = finish_list.pop(0)
         if check_end == 0:
             if finish_count != len(finish_list):
-                print('\033[31m\033[1mОшибка форматирования.\033[0m\nВ строке под номером 1 указано \033[31m\033[1mНеверное количество элементов.\033[0m')
+                print('\033[31m\033[1mОшибка форматирования.\033[0m\n'
+                      'В строке под номером 1 указано \033[31m\033[1mНеверное количество элементов.\033[0m')
             else:
                 print(finish_list)
     finally:
         text.close()
-
