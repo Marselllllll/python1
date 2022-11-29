8a9f519ddeb45627b131fc635dbbd7c34a726ab0
Marselllllll <night.square679@mail.ru>
2022-11-29 04:39:23 +0300

    complete task

--- PackForMiracle/Game.py
+++ PackForMiracle/Game.py
@@ -1,34 +1,46 @@
-def game(word, lvl):
+import os
+
+
+def Game(word: str, lvl: int) -> int:
+    """
+    :param word: Параметр (type: str), подающий на вход слово для игры.
+    :param lvl: Параметр (type: int), подающий на вход выбранный уровень для игры.
+    :return: Значение (type: int), возвращаемое функцией для дайнейшего применения в выборе (1 или 2).
+    """
     if lvl == 1:
         lvl = 7
     elif lvl == 2:
         lvl = 5
     else:
         lvl = 3
-    a = list(word)
-    c = ['\u25A0']*len(a)
-    while lvl != 0 and c != a:
-        for i in c:
+    lst: list = list(word)
+    secret: list = ['\u25A0']*len(lst)
+    while lvl != 0 and secret != lst:
+        for i in secret:
             print(i, end=' ')
         print(f'❤x{lvl}')
-        m = input("Введите букву или слово целиком: ").upper()
-        if m == word:
-            c = a
-        elif a.count(m) > 0:
-            c.pop(a.index(m))
-            c.insert(a.index(m), m)
+        vvod: str = input("Введите букву или слово целиком: ").upper()
+        if vvod == word:
+            secret = lst
+        elif lst.count(vvod) > 0:
+            secret.pop(lst.index(vvod))
+            secret.insert(lst.index(vvod), vvod)
         else:
             print('\033[31m Неправильно\033[0m, прощайся с жизнью, котик.')
             lvl -= 1
-    if c == a:
+    if secret == lst:
         print('Выиграл!')
         return 0
     else:
         print('Проиграл!')
         return 1
 
-def record(rec, lvl):
-    with open('Record.txt', encoding='cp1251', mode="w+") as text:
-        zn = f"Рекорд: {rec}; Сложность: {lvl} "
-        text.write(zn)
-
+def Record(rec: int, lvl: int) -> None:
+    """
+    :param rec: Параметр (type: int), подающий на вход рекорд.
+    :param lvl: Параметр (type: int), подающий на вход уровень рекорда.
+    :return: None
+    """
+    with open(os.path.dirname(__file__) + '\\Record.txt', mode="w+", encoding='UTF-8') as text:
+        zapis: str = f"Рекорд: {rec}; Сложность: {lvl} "
+        text.write(zapis)
