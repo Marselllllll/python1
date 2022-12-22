46dfe1673d627b1521e847bbf5bbb4dcaa4162e4
Marselllllll <night.square679@mail.ru>
2022-12-22 12:19:55 +0300

    complete task

--- Tasks/Practical/Practical_6.py
+++ Tasks/Practical/Practical_6.py
@@ -4,9 +4,11 @@ import re
 
 def find(string: str):
     name = re.findall(r'(?<=Рейс ).*', string)
+    text = open("New_journal.txt", mode="w")
     for i in name:
         all_info = i.split(" ")
-        print(f'[{all_info[5]}]: Поезд №{all_info[0]} {all_info[2]} {all_info[3]}')
+        text.write(f'[{all_info[5]}]: Поезд №{all_info[0]} {all_info[2]} {all_info[3]}\n')
+    text.close()
 
 
 with open("journal.txt", encoding="UTF8") as text:
